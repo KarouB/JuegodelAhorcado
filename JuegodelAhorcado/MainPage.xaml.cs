f90e50d97cca7ea4d020b77d967013114005f941
Samantha Cruz <samantha.j.cruz@outlook.es>
2023-02-16 16:55:08 -0300

    Creando las palabras a adivinar y los botones

--- JuegodelAhorcado/MainPage.xaml.cs
+++ JuegodelAhorcado/MainPage.xaml.cs
@@ -1,14 +1,84 @@
-﻿namespace JuegodelAhorcado;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.IO.Pipes;
 
-public partial class MainPage : ContentPage
+namespace JuegodelAhorcado;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
-	int count = 0;
+    #region UI Properties
+    public string SpotLight
+    {
+        get => spotLight;
+        set
+        {
+            spotLight = value;
+            OnPropertyChanged();
+        }
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public List<char> Letter
+    {
+        get => letter; 
+        set
+        {
+            letter = value;
+            OnPropertyChanged();
+        }
+    }
 
-	
+
+    #endregion
+
+    #region Fields
+
+    List<string> words = new List<string>()
+    {
+        "phyton",
+        "javascript",
+        "maui",
+        "csharp",
+        "sql",
+        "xalm",
+        "code",
+        "developer"
+    };
+
+    string answer = "";
+
+    private string spotLight;
+
+    List<char> guessed = new List<char>();
+
+    private List<char> letter = new List<char>();
+
+    #endregion
+
+    public MainPage()
+    {
+        InitializeComponent();
+        Letter.AddRange("abcdefghijklmnopqrstuvwxyz");
+        BindingContext = this;
+        PickWord();
+        CalculateWord(answer, guessed);
+
+    }
+
+    #region Game Engine
+
+    private void PickWord()
+    {
+        answer =
+            words[new Random().Next(0, words.Count)];
+
+        Debug.WriteLine(answer);
+    }
+    private void CalculateWord(string answer, List<char> guessed)
+    {
+        var temp = answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_')).ToArray();
+        SpotLight = string.Join(' ', temp);
+    }
+
+    #endregion
 }
 
